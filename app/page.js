14da7e2e8b57276a7c7bbcb774d1e5daa9a2bae6
akinlope <akinrimisitt@gmail.com>
2024-02-08 03:29:51 -0600

    Push for test

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
 "use client"
 
 import React from 'react'
-import Home from "./home/Home"
+// import Home from "./home/Home"
 import { Toaster } from 'react-hot-toast'
 import Navbar from './components/Navbar'
+import Home from './home/home'
 
 
 export default function page() {
   return (
     <div className='' >
       {/* <Navbar /> */}
-      <Home />     
+      <Home/>     
       <Toaster />
     </div>
   )
